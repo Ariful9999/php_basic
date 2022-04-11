6d7090af43f8ac1e3cbfc7f3e10f3ad0563c209c
Ariful9999 <102545980+Ariful9999@users.noreply.github.com>
2022-04-11 23:15:21 +0600

    Index.php

---
+++ php-basic/index.php
@@ -0,0 +1,37 @@
+<?php
+
+//  Statements
+// echo "Hellow World!";
+// print('Hellow World!');
+
+
+// Variables
+
+/* $name = "Ariful Haque";
+
+$my_parmanent_address = "Ghatail, Tangail.";
+$my_current_address = "Tangail, Bangladesh.";
+echo $name . '<br>';
+echo $my_parmanent_address . '<br>';
+echo $my_current_address; */
+
+// Data types
+
+$name = "Ariful Haque"; // String
+$age = 34; // Integer
+$height = 5.4; // Float
+$cheak = true; // Boolean
+$null_data = NULL; // Null
+
+//echo "$name <br>";
+var_dump($name);
+var_dump($age);
+var_dump($height);
+var_dump($cheak);
+var_dump($null_data);
+
+//echo $age .'<br>';
+//echo $height . '<br>';
+//echo $cheak . '<br>';
+//echo $null_data;
+
